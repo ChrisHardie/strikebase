db92abdd11d2fdf70f4e3462f60c43de278318ab
sarah <sarah@triggersandsparks.com>
2016-09-30 17:09:27 +0100

    Nix the site footer.

--- components/footer/site-info.php
+++
@@ -1,5 +0,0 @@
-<div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'strikebase' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'strikebase' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'strikebase' ), 'strikebase', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-</div><!-- .site-info -->
--- footer.php
+++ footer.php
@@ -10,12 +10,9 @@
  */
 
 ?>
+	</div><!-- #content -->
+</div><!-- #page -->
 
-	</div>
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php get_template_part( 'components/footer/site', 'info' ); ?>
-	</footer>
-</div>
 <?php wp_footer(); ?>
 
 </body>
