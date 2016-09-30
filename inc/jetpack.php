7a4e5bf772875d1dcedde2b0cabf9a6b0bc3c681
sarah <sarah@triggersandsparks.com>
2016-09-30 17:57:33 +0100

    Nix social menu.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -11,7 +11,6 @@
  * Jetpack setup function.
  *
  * See: https://jetpack.me/support/infinite-scroll/
- * See: https://jetpack.me/support/responsive-videos/
  */
 function strikebase_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
@@ -20,13 +19,6 @@ function strikebase_jetpack_setup() {
 		'render'    => 'strikebase_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
-
-	// Add theme support for Responsive Videos.
-	add_theme_support( 'jetpack-responsive-videos' );
-
-	// Add theme support for Social Menus
-	add_theme_support( 'jetpack-social-menu' );
-
 }
 add_action( 'after_setup_theme', 'strikebase_jetpack_setup' );
 
@@ -41,13 +33,5 @@ function strikebase_infinite_scroll_render() {
 		else :
 			get_template_part( 'components/post/content', get_post_format() );
 		endif;
-	}
-}
-
-function strikebase_social_menu() {
-	if ( ! function_exists( 'jetpack_social_menu' ) ) {
-		return;
-	} else {
-		jetpack_social_menu();
 	}
 }
