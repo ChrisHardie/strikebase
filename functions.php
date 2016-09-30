791e05f71ac89e31649d1f29c7434efbdca6dad0
sarah <sarah@triggersandsparks.com>
2016-09-30 17:52:31 +0100

    Nix the sidebar and widgets.

--- functions.php
+++ functions.php
@@ -106,24 +106,6 @@ function strikebase_the_custom_logo() {
 }
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function strikebase_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'strikebase' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'strikebase_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function strikebase_scripts() {
--- page.php
+++ page.php
@@ -33,5 +33,4 @@ get_header(); ?>
 		</main>
 	</div>
 <?php
-get_sidebar();
 get_footer();
