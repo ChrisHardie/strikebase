a408e3b21a873353785638dfb1115f07e5e852d1
Daniel W. Robert <danielwrobert@gmail.com>
2016-10-12 16:03:58 -0400

    Updated Gulp build tasks
    - Added JS Hint task to alert on error.
    - Added task to styles to notify oon error.

--- gulpfile.js
+++ gulpfile.js
@@ -21,13 +21,13 @@ var svgstore = require( 'gulp-svgstore' );
 gulp.task( 'styles', function() {
 	return gulp.src( 'assets/stylesheets/style.scss' )
 		.pipe( sourcemaps.init() )
-		.pipe( sass( { style: 'expanded' } ).on( 'error', sass.logError ) )
+		.pipe( sass( { style: 'expanded' } ) )
+		.on( 'error', notify.onError( function( err ) {
+			return "Stylesheet Error in " + err.message;
+		} ) )
 		.pipe( autoprefixer( { browsers: ['last 2 versions', 'ie >= 9'], cascade: false } ) )
 		//.pipe( csscomb() )
 		.pipe( sourcemaps.write( './', { includeContent: false, sourceRoot: 'source' } ) )
-		.on( 'error', function ( err ) {
-			console.error( 'Error!', err.message );
-		} )
 		.pipe( gulp.dest( './' ) )
 		.pipe( livereload() );
 });
@@ -35,6 +35,7 @@ gulp.task( 'styles', function() {
 // Scripts
 gulp.task( 'scripts', function() {
 	return gulp.src( 'assets/js/*.js' )
+		.pipe( jshint() )
 		.pipe( jshint.reporter( 'default' ) )
 		//.pipe( concat( 'main.js' ) )
 		.pipe( gulp.dest( 'assets/js' ) );
