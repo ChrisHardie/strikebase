d3b3f450062fd13ba1447fb2883a05ff19bb1dc8
sarah <sarah@triggersandsparks.com>
2016-10-30 20:24:04 +0000

    Configure timezone input as an autocomplete.
    In order to ensure we have consistent data for a person's timezone, we're using a list of PHP-supported timezones.
    This should make it easier for PHP to handle the output of the field.
    
    The timezone list as output by PHP is suuuuuper long, so I've configured this as an autocomplete for the moment.
    This works nicely if you're in a major city, but is obviously less ideal if you're not. (For instance, if I enter
    "London" it gives me London, but "Edinburgh" won't work.) Guessing in these cases is often easier with a giant
    select box, but an autocomplete is easier to use if you know the best major city nearby to code it to.
    
    Additionally, the output is currently using underscores, so a search for "New York" won't return any results.
    Pretty easy fix if we decide an autocomplete is the better option here, although I'm not convinced it is.
    
    Fixes #21.

--- inc/cpt/people/class-people-custom-fields.php
+++ inc/cpt/people/class-people-custom-fields.php
@@ -34,6 +34,14 @@ class Strikebase_Person_Fields {
 		add_action( 'fm_post_person', array( $this, 'add_person_fields' ) );
 	}
 
+	/*
+	 * Get a list of all the timezones PHP supports.
+	 */
+	private function get_timezones() {
+		$timezone_list = DateTimeZone::listIdentifiers( DateTimeZone::ALL );
+		return $tzlist;
+	}
+
 	/**
 	 * Add custom fields for Person post type
 	 *
@@ -57,9 +65,12 @@ class Strikebase_Person_Fields {
 					'name'  => 'phone',
 					'label' => esc_html__( 'Phone Number(s)', 'strikebase' ),
 				) ),
-				'time_zone' => new Fieldmanager_Textfield( array(
+				'time_zone' => new Fieldmanager_Autocomplete( array(
 					'name'  => 'time_zone',
-					'label' => esc_html__( 'Timezone', 'strikebase' ),
+					'label' => esc_html__( 'Time Zone', 'strikebase' ),
+					'datasource'  => new Fieldmanager_Datasource( array(
+						'options' => self::get_timezones(),
+					) ),
 				) ),
 
 				// Social media accounts.
