367043f0084f8f22c6430a81db87fcca76fb0ace
Daniel W. Robert <danielwrobert@gmail.com>
2016-11-01 11:23:51 -0400

    Added in custom WP_Query to connect project list to person.

--- components/person/content-full.php
+++ components/person/content-full.php
@@ -23,28 +23,28 @@
 
 		<dt><?php esc_html_e( 'Projects', 'strikebase' ); ?></dt>
 		<?php
-		$projects = strikebase_get_project_meta( get_the_ID(), 'people' );
+		// Custom query setup
+		global $person_page_id;
+		$person_page_id = get_the_id();
+		$query_args = array( 'post_type' => 'project', 'posts_per_page' => -1 );
+		$project_query = new WP_Query( $query_args );
 
-		var_dump( get_the_ID() );
+		// Custom query loop
+		if ( $project_query->have_posts() ) {
+			while ( $project_query->have_posts() ) {
+				$project_query->the_post();
+				$projects = strikebase_get_project_meta( get_the_ID(), 'people' );
 
-		if ( $projects ) :
-			foreach ( $projects as $key => $value ) :
-				if ( $value ) :
-					echo '<dt>' . strikebase_nice_key( $key ) . '</dt>';
-					if ( is_array( $value ) ) :
-						// If our value is an array, loop through it as well!
-						foreach ( $value as $person ) :
-							if ( get_the_title() === get_the_title( $person ) ) :
-								echo '<dd>' . get_the_title( $person ) . '</dd>';
-							endif;
-						endforeach;
-					else :
-						// Otherwise just output the value.
-						echo '<dd>' . $value . '</dd>';
-					endif;
-				endif;
-			endforeach;
-		endif;
+				if ( $projects ) {
+					if ( in_array( $person_page_id, $projects['influencers'] ) ) {
+						echo '<dd>' . get_the_title() . '</dd>';
+					}
+				}
+			}
+			wp_reset_postdata();
+		} else {
+			echo '<dd>No associated projects.</dd>';
+		}
 		?>
 	</dl>
 
