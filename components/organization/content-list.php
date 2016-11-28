f675763c03ccce1c2292f0f1f1515002aa58cfe9
sarah <sarah@triggersandsparks.com>
2016-11-28 20:33:30 +0000

    Add check-in date to org page. Fixes #47.

--- components/organization/content-list.php
+++ components/organization/content-list.php
@@ -24,7 +24,7 @@
 	</td>
 
 	<td class="strikebase-person-last-contact">
-		<strong>[LAST CONTACT DATE]</strong>
+		<?php strikebase_show_checkin_date( $term->slug ); ?>
 	</td>
 
 </tr><!-- #post-## -->
