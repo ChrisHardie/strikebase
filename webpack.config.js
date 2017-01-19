f1e684cbef8dc2729837a598be4709285dee79f0
Daniel W. Robert <danielwrobert@gmail.com>
2017-01-19 09:21:44 -0500

    Renamed main App.js file to index.js - main entry point, not a component.

--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,63 @@ const svgFragments = require( 'postcss-svg-fragments' );
 const path = require( 'path' );
 
 module.exports = {
-  entry: {
-    app: [
-      './assets/js/src/App.js'
-    ]
-  },
+	entry: {
+		app: [
+			'./assets/js/src/index.js'
+		]
+	},
 
-  output: {
-	path: __dirname,
-    publicPath: '/',
-    filename: '/assets/js/main.js',
-    sourceMapFilename: 'main.js.map'
-  },
+	output: {
+		path: __dirname,
+		publicPath: '/',
+		filename: '/assets/js/main.js',
+		sourceMapFilename: 'main.js.map'
+	},
 
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.es6'],
-    modulesDirectories: ['node_modules']
-  },
+	resolve: {
+		extensions: ['', '.js', '.jsx', '.es6'],
+		modulesDirectories: ['node_modules']
+	},
 
-  module: {
-    preloaders: [
-      { test: /\.jsx$|\.es6$|\.js$/, loader: 'source-map', query: { presets: ['react', 'latest'] }, exclude: /(node_modules|bower_components)/ }
-    ],
-    loaders: [
-      { test: /\.jsx$|\.es6$|\.js$/, loader: 'babel', query: { presets: ['react', 'es2015'] }, exclude: /(node_modules|bower_components)/ },
-      { test: /\.scss$|\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
-      { test: /.*\.(gif|png|jpe?g|svg)$/i, loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=5'] }
-    ]
-  },
+	module: {
+		preloaders: [
+			{
+				test: /\.jsx$|\.es6$|\.js$/,
+				loader: 'source-map',
+				query: { presets: ['react', 'latest'] },
+				exclude: /(node_modules|bower_components)/
+			}
+		],
+		loaders: [
+			{
+				test: /\.jsx$|\.es6$|\.js$/,
+				loader: 'babel',
+				query: { presets: ['react', 'es2015'] },
+				exclude: /(node_modules|bower_components)/
+			},
+			{
+				test: /\.scss$|\.css$/,
+				loader: ExtractTextPlugin.extract( 'style-loader', 'css-loader?sourceMap!postcss-loader!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true' )
+			},
+			{
+				test: /.*\.(gif|png|jpe?g|svg)$/i,
+				loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=5']
+			}
+		]
+	},
 
-  devtool: "eval",
+	devtool: "eval",
 
-  postcss: function() {
-    return [
-	autoprefixer( { browsers: ['last 2 versions'] } ),
-	svgFragments
-    ];
-  },
+	postcss: function() {
+		return [
+			autoprefixer( { browsers: ['last 2 versions'] } ),
+			svgFragments
+		];
+	},
 
-  plugins: [
-    new ExtractTextPlugin( 'style.css', {
-      allChunks: true
-    } )
-  ]
+	plugins: [
+		new ExtractTextPlugin( 'style.css', {
+			allChunks: true
+		} )
+	]
 };
