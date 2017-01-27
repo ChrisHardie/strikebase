66e9a31434eda8b057780968e02e623f66d6f3fe
Daniel W. Robert <danielwrobert@gmail.com>
2017-01-27 15:52:54 -0500

    Re-added class prefix to class files - appears to be a WP convention, looking at Core files.

---
+++ inc/custom-taxonomies/custom-taxonomies-register.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Init Custom Taxonomies
+ */
+function strikebase_taxonomies_init() {
+	// General class
+	require_once( get_template_directory() . '/inc/custom-taxonomies/class-taxonomies.php' );
+	// Classes for individual Taxonomies
+	require_once( get_template_directory() . '/inc/custom-taxonomies/class-taxonomies-person.php' );
+	require_once( get_template_directory() . '/inc/custom-taxonomies/class-taxonomies-project.php' );
+	require_once( get_template_directory() . '/inc/custom-taxonomies/class-taxonomies-shared.php' );
+}
+
+add_action( 'after_setup_theme', 'strikebase_taxonomies_init' );
