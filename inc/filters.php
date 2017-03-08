e41bc03b17496783dccc91c72e5a522e20fec5bb
sarah <sarah@triggersandsparks.com>
2017-03-08 20:51:23 +0000

    Allow for the hierarchical display of taxonomies in filter lists.

--- inc/filters.php
+++ inc/filters.php
@@ -11,7 +11,7 @@
  */
 function strikebase_filters( $taxonomy, $dropdown_label ) { ?>
 
-	<ul class="dropdown-container">
+	<ul class="dropdown-container dropdown-container-<?php echo $taxonomy; ?>">
 
 		<li class="dropdown-label">
 			<a href="#"><?php echo esc_html__( $dropdown_label ); ?></a>
@@ -19,20 +19,43 @@ function strikebase_filters( $taxonomy, $dropdown_label ) { ?>
 			<ul class="dropdown">
 				<li class="dropdown-title"><?php printf( esc_html__( 'Filter by %1$s', 'strikebase' ), strtolower( get_taxonomy( $taxonomy )->label ) ); ?></li>
 
-				<?php
-				// Get all terms in our taxonomy.
-				$terms = get_terms( array(
-					'taxonomy'   => $taxonomy,
-					'hide_empty' => false,
-				) );
-
-				// Loop through the terms and output each as a filter option.
-				foreach ( $terms as $term ) : ?>
-					<li><a class="filter-link" href="#" data-filter="<?php echo $term->slug; ?>"><?php echo $term->name; ?></a></li>
-				<?php endforeach; ?>
+				<?php strikebase_filter_terms( $taxonomy ); ?>
 			</ul> <!-- .dropdown -->
 		</li>
 	</ul><!-- .dropdown-container -->
 
 <?php
+}
+
+function strikebase_filter_terms( $taxonomy, $parent=0 ) {
+	// Get all terms in our taxonomy.
+	$terms = get_terms( array(
+		'taxonomy'   => $taxonomy,
+		'hide_empty' => false,
+		'parent'     => $parent,
+	) );
+
+	if ( $terms ) :
+		if ( 0 !== $parent ) : ?>
+			<ul class="dropdown-submenu">
+		<?php endif;
+
+			// Loop through the terms and output each as a filter option.
+			foreach ( $terms as $term ) : ?>
+				<li><a class="filter-link" href="#" data-filter="<?php echo $term->slug; ?>"><?php echo $term->name; ?></a>
+
+				<?php
+				// Check for child terms of the current taxonomy as well!
+				strikebase_filter_terms( $taxonomy, $term->term_id );
+				?>
+
+				</li>
+
+			<?php
+			endforeach;
+
+		if ( 0 !== $parent ) : ?>
+			</ul>
+		<?php endif;
+	endif;
 }
