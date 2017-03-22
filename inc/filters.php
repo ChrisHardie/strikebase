03f3e121b19371468a9ad5f70c4021be45d8a0c8
sarah <sarah@triggersandsparks.com>
2017-03-22 12:24:32 +0000

    Replace placeholder carets with SVGs.

--- inc/filters.php
+++ inc/filters.php
@@ -14,7 +14,8 @@ function strikebase_filters( $taxonomy, $dropdown_label ) { ?>
 	<ul class="dropdown-container dropdown-container-<?php echo $taxonomy; ?>" data-taxonomy-group="<?php echo $taxonomy; ?>">
 
 		<li class="dropdown-label">
-			<a class="dropdown-link" href="#"><?php echo esc_html__( $dropdown_label ); ?><img class="strikebase-icon" src="https://icon.now.sh/chevron/down" alt="Show filter options" /></a>
+			<a class="dropdown-link" title="<?php esc_html_e( 'Show filtering options', 'strikebase' ); ?>" href="#"><?php echo esc_html__( $dropdown_label ); ?><?php strikebase_icon( 'caret' ); ?>
+			</a>
 
 			<div class="dropdown-wrapper">
 			<ul class="dropdown-menu dropdown-main-menu">
@@ -43,7 +44,7 @@ function strikebase_filters( $taxonomy, $dropdown_label ) { ?>
 							$child_term_array[$term->slug] = strikebase_filter_child_terms( $taxonomy, $term->term_id );
 							?>
 
-							<li><a class="dropdown-submenu-link" href="#" data-target="<?php echo $term->slug; ?>"><?php echo $term->name; ?><img class="strikebase-icon" src="https://icon.now.sh/chevron" alt="Show sub-categories" /></a></li>
+							<li><a class="dropdown-submenu-link" href="#" data-target="<?php echo $term->slug; ?>" <?php esc_html_e( 'Show subcategories', 'strikebase' ); ?>><?php echo $term->name; ?><?php strikebase_icon( 'caret' ); ?></a></li>
 
 						<?php else:
 							strikebase_filter_link( $term->taxonomy, $term->slug, $term->name );
@@ -58,7 +59,7 @@ function strikebase_filters( $taxonomy, $dropdown_label ) { ?>
 				if ( ! empty( $child_term_array ) ) :
 					foreach ( $child_term_array as $parent_term=>$child_terms ) : ?>
 						<ul class="dropdown-menu dropdown-submenu <?php echo $parent_term; ?>">
-							<li class="dropdown-title"><a class="dropdown-submenu-close" href="#" data-target="<?php echo $parent_term; ?>"><img class="strikebase-icon" src="https://icon.now.sh/chevron/left" alt="Back to parent category" /><?php echo $parent_term; ?></a></li>
+							<li class="dropdown-title"><a class="dropdown-submenu-close" href="#" data-target="<?php echo $parent_term; ?>" title="<?php esc_html_e( 'Back to parent category', 'strikebase' ); ?>"><?php strikebase_icon( 'caret' ); ?><?php echo $parent_term; ?></a></li>
 
 							<?php foreach ( $child_terms as $child_term ) :
 								strikebase_filter_link( $term->taxonomy, $child_term->slug, $child_term->name );
