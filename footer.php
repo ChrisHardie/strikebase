a37c14a6930310f8e333589c6bd1e7b6848d7135
sarah <sarah@triggersandsparks.com>
2017-03-27 16:43:41 +0100

    Add some texture to the background.

--- footer.php
+++ footer.php
@@ -12,8 +12,6 @@
 ?>
 	</div><!-- #content -->
 	<?php strikebase_svg( get_template_directory_uri() . '/assets/svg/strikebase.svg' ); ?>
-
-	<!--	<text class="logotype" transform="translate(122.98 277.34)"><tspan class="strike">Strike</tspan><tspan class="base">base</tspan></text> -->
 </div><!-- #page -->
 
 
