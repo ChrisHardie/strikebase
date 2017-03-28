64689e7c243716d2b5837999cbdd0810cb91d9d9
Daniel W. Robert <danielwrobert@gmail.com>
2017-03-28 11:22:28 -0400

    Added Chrome's Redux Dev-Tools to store.

--- assets/js/src/store.js
+++ assets/js/src/store.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import thunk from 'redux-thunk';
@@ -11,8 +11,14 @@ import thunk from 'redux-thunk';
  */
 import rootReducer from './reducers/root';
 
+// Create Store enhancers
+const enhancers = compose(
+	applyMiddleware( thunk ),
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 // Create Store
-const store = createStore( rootReducer, applyMiddleware( thunk ) );
+const store = createStore( rootReducer, enhancers );
 
 // Sync browser history and export to make available to other modules (not as default)
 export const history = syncHistoryWithStore( browserHistory, store );
