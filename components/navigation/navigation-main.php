dbfd9b957fd828a2084ae00242eb44b26aa3a36a
sarah <sarah@triggersandsparks.com>
2017-03-29 01:54:34 +0100

    Add projects icon. Thanks AI for the mental breakdown!

--- components/navigation/navigation-main.php
+++ components/navigation/navigation-main.php
@@ -11,7 +11,7 @@
 <nav id="site-navigation" class="main-navigation" role="navigation">
     <ul>
         <li><a href="/"><?php strikebase_icon( 'dashboard' ); ?><?php esc_html_e( 'Dashboard', 'strikebase' ); ?></a></li>
-        <li><a href="/projects"><?php esc_html_e( 'Projects' ); ?></a></li>
+        <li><?php strikebase_icon( 'projects' ); ?><a href="/projects"><?php esc_html_e( 'Projects' ); ?></a></li>
         <li class="context-link"><?php strikebase_primary_link(); ?></li>
         <li><a href="/people"><?php esc_html_e( 'People', 'strikebase' ); ?></a></li>
         <li><a href="/organizations"><?php esc_html_e( 'Organizations', 'strikebase' ); ?></a></li>
