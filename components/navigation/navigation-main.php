3cb719dbce2a7dd71dd78635cc46334ee2ebb73b
sarah <sarah@triggersandsparks.com>
2017-04-01 14:19:34 +0100

    Add organizations icon.

--- components/navigation/navigation-main.php
+++ components/navigation/navigation-main.php
@@ -14,6 +14,6 @@
         <li><?php strikebase_icon( 'projects' ); ?><a href="/projects"><?php esc_html_e( 'Projects' ); ?></a></li>
         <li class="context-link"><?php strikebase_primary_link(); ?></li>
         <li><?php strikebase_icon( 'people' ); ?><a href="/people"><?php esc_html_e( 'People', 'strikebase' ); ?></a></li>
-        <li><a href="/organizations"><?php esc_html_e( 'Organizations', 'strikebase' ); ?></a></li>
+        <li><?php strikebase_icon( 'organizations' ); ?><a href="/organizations"><?php esc_html_e( 'Organizations', 'strikebase' ); ?></a></li>
     </ul>
 </nav>
